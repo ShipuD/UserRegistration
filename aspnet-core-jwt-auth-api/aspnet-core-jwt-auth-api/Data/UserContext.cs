8d8d9ea1d14a9404dfb7b72869267038f56fbf38
ShipuD <shipu4@gmail.com>
2020-07-24 22:34:52 +0100

    Initial API proj files

---
+++ aspnet-core-jwt-auth-api/aspnet-core-jwt-auth-api/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using aspnet_core_jwt_auth_api.Models;
+
+namespace aspnet_core_jwt_auth_api.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<aspnet_core_jwt_auth_api.Models.User> User { get; set; }
+    }
+}
